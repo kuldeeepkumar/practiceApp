58dee8ee976de4a433f9d48020420325dc56f94c
deepak9876462954 <bhatiadeep32@gmail.com>
2024-06-11 15:36:36 +0530

    concatination

--- App.js
+++ App.js
@@ -2,8 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const a=6;
+  const b=7;
+  const c=a+b;
+  console.log("the sum is:",c);
   return (
     <View style={styles.container}>
+
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
