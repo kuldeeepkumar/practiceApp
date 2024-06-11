5464f375361978500118da02370c495d3985caa9
amanjotkaur9115 <amanjotkaur1746>
2024-06-11 15:43:51 +0530

    suym

--- App.js
+++ App.js
@@ -2,9 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const a=10;
+  const b=20;
+  const c=a+b;
+  console.log(c);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>happy birthday !</Text>
       <StatusBar style="auto" />
     </View>
   );
