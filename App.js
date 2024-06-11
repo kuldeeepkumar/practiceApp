02d5224c0854b3a7744571025d488de7c6d63057
pinkuydevi12 <pinkybhatia48gmail.com>
2024-06-11 15:36:52 +0530

    change

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-web';
 
 export default function App() {
+  let b = "mehaku";
+  let c = "priyanka";
+
+  console.log("my console is Here:",b,c)
+const  x=10;
+const y=20;
+sum=x+y;
+console.log("here is sum:",x+y);
+console.log("here is subtraction:",x-y);
+console.log("here is multiplication:",x*y);
+console.log("here is divison:",x/y);
+console.log("here is modules:",x%y);
+console.log("here is:",x>y);
+console.log("here is:",x<y);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>pinky Bhatia</Text>
+      <Text>akshu</Text>
       <StatusBar style="auto" />
     </View>
   );
