f2044557e5a6ae78500ad7b061f68d0410e95299
deepak9876462954 <bhatiadeep32@gmail.com>
2024-06-12 14:05:55 +0530

    change

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const a=6;
+  const a=8;
   const b=7;
   const c=a+b;
   console.log("the sum is:",c);
