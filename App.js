592c69446827c38dc4d8fd30a8d21c69201697ae
deepak9876462954 <bhatiadeep32@gmail.com>
2024-06-13 12:18:49 +0530

    arithematic operator

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Excercise1 from './src/exercises/Excercise1';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Excercise1 from "./src/exercises/Excercise1";
 
 export default function App() {
-  let a=8;
+  let a = 8;
   console.log(+a);
   console.log(-a);
   console.log(++a);
   console.log(--a);
   return (
     <>
-    <View style={styles.container}>
-
-      <Text>hello guys</Text>
-      <StatusBar style="auto" />
-    </View>
-    <Excercise1/>
+      <View style={styles.container}>
+        <Text>hello guys</Text>
+        <StatusBar style="auto" />
+      </View>
+      <Excercise1 />
     </>
   );
 }
@@ -23,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
