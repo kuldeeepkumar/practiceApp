ff55292cdb54482991c46a8e62dee8a983fd370e
sandhushweta <sandhushweta1408>
2024-06-13 09:57:04 +0530

    arithematic

--- App.js
+++ App.js
@@ -2,8 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const a=7;
-  console.log("value:",a);
+  const a=10;
+  const b=5;
+  const c=a+b;
+  const d=a-b;
+  const e=a*b;
+  const f=a/b;
+  const g=a%b;
+
+  console.log("value:",c);
+  console.log("value:",d);
+  console.log("value:",e);
+  console.log("value:",f);
+  console.log("value:",g);
   return (
     <View style={styles.container}>
       <Text>ki hall chal hai dosto!</Text>
