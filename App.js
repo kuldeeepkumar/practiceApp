2cf1afc6c3debcfcb0a08fe1c737efc228a1cb39
pinkydevi12 <pinkybhatia48@gmail.com>
2024-06-13 11:57:27 +0530

    changes

--- App.js
+++ App.js
@@ -1,36 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-web';
+import Exercise1 from './src/exercises/exercise1';
 
 export default function App() {
   let b = "mehaku";
    console.log("my besti name:",b)
-const  x=20;
-const y=30;
-const a=6;
+let  x=20;
+let y=30;
+let a=6;
 console.log("here is sum:",x+y);
 console.log("here is subtraction:",x-y);
 console.log("here is multiplication:",x*y);
 console.log("here is divison:",x/y);
 console.log("here is modules:",x%y);
-console.log("here is:",x>y);
-console.log("here is:",x<y);
-console.log("here is:",+a);
-console.log("here is:",-a);
+console.log("greater than:",x>y);
+console.log("less than:",x<y);
+console.log("equal to:",x==y);
+console.log("not equal to:",x!=y);
+console.log("here is unary plus:",+a);
+console.log("here is unary minus:",-a);
+console.log("here is increment:",++a);
+console.log("here is:decremant",--a);
 let datevar=new Date();
 console.log(datevar);
 let objectvar={name:"pinky",frnd:"ashu"};
 console.log(objectvar);
 let arrayvar=[1,2,3,4];
 console.log(arrayvar);
-
+let numbervar=234566;
+let stringvar="akshu";
+let booleanvar=true;
+let nullvar=null ;
+console.log(numbervar);
+console.log(stringvar);
+console.log(booleanvar);
+console.log(nullvar);
+let p=10;
+console.log(p+=5);
+console.log(p-=5);
+console.log(p*=1);
+console.log(p/=4);
+let k=true;
+let l=false;
+console.log(k&&l);
+console.log(k||l);
+console.log(!k);
 
 return (
-    <View style={styles.container}>
-      <Text>aman</Text>
-      <Text>nav</Text>
+<>
+<View style={styles.container}>
+      <Text>jass</Text>
+      <Text>pinko</Text>
+      <Text>ashuu</Text>
       <StatusBar style="auto" />
     </View>
+    <Exercise1/>
+</>
   );
 }
 
---
+++ src/exercises/Exercise1.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Exercise1 = () => {
+    const a = "pinky"
+
+    console.log("value" , a);
+    
+    
+  return (
+    <View>
+      <Text>Exercise1</Text>
+    </View>
+  )
+}
+
+export default Exercise1
+
+const styles = StyleSheet.create({})
