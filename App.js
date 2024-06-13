fc89d3e1802ba46e020a6d36855a7b912ec99fb7
deepak9876462954 <bhatiadeep32@gmail.com>
2024-06-13 11:56:35 +0530

    assignment operator

--- App.js
+++ App.js
@@ -1,23 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Excercise1 from './src/exercises/Excercise1';
 
 export default function App() {
-  const a=8;
-  const b=7;
-  const c=a+b;
-  const d=a-b;
-  const e=a*b;
-  const f=a/b;
-  console.log("the sum is:",c);
-  console.log("the difference is:",d);
-  console.log("the multiplication is:",e);
-  console.log("the division is:",f);
+  let a=8;
+  console.log(+a);
+  console.log(-a);
+  console.log(++a);
+  console.log(--a);
   return (
+    <>
     <View style={styles.container}>
 
       <Text>hello guys</Text>
       <StatusBar style="auto" />
     </View>
+    <Excercise1/>
+    </>
   );
 }
 
