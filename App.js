5cf9188c0b633eaef940e8a483c829982c909848
Kuldeep <pro.kuldeepkumar@gmail.como>
2024-06-13 14:58:11 +0530

    conditional statements and looping statements are done

--- App.js
+++ App.js
@@ -24,36 +24,6 @@ export default function App() {
   const d = true;
   console.log("sum is here: ", t && d);
 
-  const colors = ["red", "green", "blue"];
-
-  // for (let color of colors) {
-  //   console.log(color);
-  // }
-  // for (let i = 0; i < 5; i++) {
-  //   console.log(i);
-  // }
-
-  // let i = 0;
-  // while (i < 5) {
-  //   console.log(i);
-  //   i++;
-  // }
-
-  // let i = 0;
-  // do {
-  //   console.log(i);
-  //   i++;
-  // } while (i < 5);
-
-  const person = {
-    name: "John",
-    age: 30,
-    city: "New York",
-  };
-
-  for (let key in person) {
-    console.log(key + ": " + person[key]);
-  }
   return (
     <>
       <View style={styles.container}>
