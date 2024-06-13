eddb0fbc8958a5469cdc7bc5241d8f395abb744a
deepak9876462954 <bhatiadeep32@gmail.com>
2024-06-13 12:16:48 +0530

    arithematic operator

--- src/exercises/Excercise1.js
+++ src/exercises/Excercise1.js
@@ -1,19 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 const Excercise1 = () => {
-    let a=8;
-    console.log(+a);
-    console.log(-a);
-    console.log(++a);
-    console.log(--a);
+  const a = 8;
+  const b = 9;
+  const c = a + b;
+  const d = a - b;
+  const e = a * b;
+  const f = a / b;
+  console.log("sum is:", c);
+  console.log("sub is:", d);
+  console.log("mul is:", e);
+  console.log("div is:", f);
+
   return (
     <View>
       <Text>Excercise1</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Excercise1
+export default Excercise1;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
