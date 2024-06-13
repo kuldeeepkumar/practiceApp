439391686a96208d6de5959374f5cda20f938bd2
sharmaaashima <aashimasharma164@gmail.com>
2024-06-13 11:56:57 +0530

    label chnages

---
+++ src.js/exercises/Exercise1.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Exercise1 = () => {
+    console.log(
+        "hello ASHIMA"
+        
+
+    )
+  return (
+    <View>
+      <Text>Exercise1</Text>
+    </View>
+  )
+}
+
+export default Exercise1
+
+const styles = StyleSheet.create({})
