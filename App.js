7dec8fa77a630b130a06c45a4669dd7b7ecdb995
Kuldeep <pro.kuldeepkumar@gmail.como>
2024-06-13 13:51:46 +0530

    some changes

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
+import Exercise1 from "./src/exercises/Exercise1";
 import { StatusBar } from "expo-status-bar";
 import { kuldeep } from "./src/components/index";
 
 export default function App() {
+  // -------------->>>>>>>>>>>>>>>>>Variable Declaration<<<<<<<<<<<<<<<<---------------
+
   let a = "kuldeep";
   let ke = 6549879;
   var b = "b or boy";
   const c = "c for cat";
   let y;
   y = "y for yellow";
+
+  const abc = {
+    name: "kuldeep",
+    rollNo: 132465,
+    addres: "India",
+  };
 
   const t = false;
   const d = true;
@@ -46,13 +55,16 @@ export default function App() {
     console.log(key + ": " + person[key]);
   }
   return (
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.button} onPress={kuldeep}>
-        <Text>Click me</Text>
-      </TouchableOpacity>
+    <>
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.button} onPress={kuldeep}>
+          <Text>Click me</Text>
+        </TouchableOpacity>
 
-      <StatusBar style="auto" />
-    </View>
+        <StatusBar style="auto" />
+      </View>
+      <Exercise1 />
+    </>
   );
 }
 
--- src/exercises/Exercise1.js
+++ src/exercises/Exercise1.js
@@ -3,6 +3,23 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 const Exercise1 = () => {
+  // -------------->>>>>>>>>>>>>>>>>Conditional Statements<<<<<<<<<<<<<<<<---------------
+
+  // If condition code is here
+  // if else condition code is here
+  // else if condition code is here
+  // switch statement code is here
+
+  // -------------->>>>>>>>>>>>>>>>>Looping Statements<<<<<<<<<<<<<<<<---------------
+
+  // for loop code is here
+  // while loop code is here
+  // do while loop code is here
+  // for in while loop code is here
+  // for of loop code is here
+
+  // -------------->>>>>>>>>>>>>>>>>Function delaration and its use <<<<<<<<<<<<<<<<---------------
+
   console.log("hello kuldeep");
   return (
     <View>
