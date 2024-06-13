b22d614beb0274f29834680eed0985de4e456f7f
deepak9876462954 <bhatiadeep32@gmail.com>
2024-06-13 12:31:11 +0530

    assignment operator

--- src/exercises/Excercise1.js
+++ src/exercises/Excercise1.js
@@ -2,16 +2,11 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 const Excercise1 = () => {
-  const a = 8;
-  const b = 9;
-  const c = a + b;
-  const d = a - b;
-  const e = a * b;
-  const f = a / b;
-  console.log("sum is:", c);
-  console.log("sub is:", d);
-  console.log("mul is:", e);
-  console.log("div is:", f);
+  let a = 8;
+  console.log((a += 6));
+  console.log((a -= 7));
+  console.log((a *= 3));
+  console.log((a /= 6));
 
   return (
     <View>
