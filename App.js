fcde54b0f3517f8210823aea4e70dc226e80b2e0
amanbhatia295 <js3251090@gmail.com>
2024-06-13 11:56:30 +0530

    label change

--- App.js
+++ App.js
@@ -1,15 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Exetcises1 from './src/Exercises/Exetcises1';
 
-export default function App() {
+export defaulta function App() {
+  let a="navi";
+  var b="a or aman";
+  console.log("my brother name:",a)
+  const x=30;
+  const y=40;
+  const z=10;
+  console.log("here is sum:",x+y);
+  console.log("here is subtrsction:",x-y);
+  console.log("here is multiplication:",x*y);
+  console.log("here is divison:",x/y);
+  console.log("here is modules:",x%y);
+  console.log("here is:",x>y);
+  console.log("here is:",x<y);
+  console.log("here is:",+z);
+  console.log("here is:",-z);
+  let datevar=new Date();
+  console.log(dataver);
+  let objectvar={name="aman",frnd:"nav"};
+console.log(objectvar);
+let arrayvar=[1,2,3,4]  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <> <View style={styles.container}>
+   <Text>open up app.js</Text>
+   <StatusBar style="auto" />
+ </View>Exetcises1
+ </>
+    
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
---
+++ src/Exercises/Exetcises1.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Exetcises1 = () => {
+    let a=10;
+    console.log(a);
+  return (
+    <View>
+      <Text>Exetcises1</Text>
+    </View>
+  )
+}
+
+export default Exetcises1
+
+const styles = StyleSheet.create({})
