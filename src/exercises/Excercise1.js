063d90a6c99c3bbff319082eb7a1aa5c68100702
deepak9876462954 <bhatiadeep32@gmail.com>
2024-06-13 11:57:05 +0530

    assignment operator

---
+++ src/exercises/Excercise1.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Excercise1 = () => {
+    console.log("hello guys")
+  return (
+    <View>
+      <Text>Excercise1</Text>
+    </View>
+  )
+}
+
+export default Excercise1
+
+const styles = StyleSheet.create({})
