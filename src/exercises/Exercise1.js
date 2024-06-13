5394ea6bbcd6fd37593c24c1f97a3c3ef28d108d
hasandeep3013 <singhhasandeep22@gmail.com>
2024-06-13 16:07:58 +0530

    new change

--- src/exercises/Exercise1.js
+++ src/exercises/Exercise1.js
@@ -2,20 +2,53 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 const Exercise1 = () => {
-  const a = 3;
-  const b = 4;
-  const c = 8;
-  const d = a + b;
-  const e = a * b * c;
-  const f = a / b / c;
-  console.log("sum is", d);
-  console.log("multiply is", e);
-  console.log("devide is", f);
-  console.log("hello hassan");
+  //....>>>>>>>>>conditional statements<<<<<<<<<<<....
+  const a = 15;
+  if (a >= 13) {
+    console.log("you are young");
+  } else {
+    console.log("you are not youmg");
+  }
+  //else if condition
+  const x = 10;
+  if (x > 14) {
+    console.log("you are poor");
+  } else if (x > 20) {
+    console.log("you are rich");
+  } else {
+    console.log("you are midel class");
+  }
+  //switch case
+  switch (a) {
+    case 15:
+      console.log("match");
+      break;
+    case 40:
+      console.log("match");
+      break;
+    default:
+      console.log("not match");
+      break;
+  }
+  //......<<<<<<<<<<<< program relate to for loop >>>>>>>>>>>......
+  for (let i = 0; i < 7; i++) {
+    console.log(i);
+  }
+  //.....<<<<<<<<<< relate to to while loop>>>>>>>>>>.....
+  let i = 6;
+  while (i <= 4) {
+    console.log(i);
+    i++;
+  }
+  //.......<<<<<<< do while loop >>>>>>......
+  do {
+    console.log(i);
+    i++;
+  } while (i <= 5);
   return (
-    <view>
-      <text>Exercise1</text>
-    </view>
+    <View>
+      <Text>Exercise 1</Text>
+    </View>
   );
 };
 
