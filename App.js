d49c4feb2f409be657934ad546131e318af880c8
gurjinder2005 <anjli3118@gmail.com>
2024-06-13 11:40:24 +0530

    operator exercise

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Exercise1 from './src/exercises/Exercise1';
 
 export default function App() {
+  let b = "manisha";
+   console.log("my name is manisha:",b)
+   let x=34;
+   let y=12;
+   let z=56;
+   console.log("here is sum:",x+y);
+   console.log("here is subtraction:",z-y);
+   console.log("here is modules:",x%z);
+   console.log("here is multi:",x*z);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+<>
+<View style={styles.container}>
+      <Text>manisha raju!</Text>
       <StatusBar style="auto" />
     </View>
+    <Exercise1/>
+</>
   );
 }
 
---
+++ src/exercises/Exercise1.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Exercise1 = () => {
+    console.log("hello gurjinder")
+    
+    
+  return (
+    <View>
+      <Text>Exercise1</Text>
+    </View>
+  )
+}
+
+export default Exercise1
+
+const styles = StyleSheet.create({})
