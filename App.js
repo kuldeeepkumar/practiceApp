e6182416601d0c8f80c9dca3aac286ad5bb29bd8
sandhushweta <sandhushweta1408>
2024-06-13 10:05:36 +0530

    assignment operator

--- App.js
+++ App.js
@@ -4,17 +4,19 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   const a=10;
   const b=5;
-  const c=a+b;
-  const d=a-b;
-  const e=a*b;
-  const f=a/b;
-  const g=a%b;
+  const c=a<b;
+  const d=a>b;
+  const e=a<=b;
+  const f=a>=b;
+  const g=a==b;
+  const h=a!=b;
 
-  console.log("value:",c);
+console.log("value:",c);
   console.log("value:",d);
   console.log("value:",e);
   console.log("value:",f);
   console.log("value:",g);
+  console.log("value:",h);
   return (
     <View style={styles.container}>
       <Text>ki hall chal hai dosto!</Text>
