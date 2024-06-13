78952c972825d6318120c80efa2d15b6a2e465e8
deepak9876462954 <bhatiadeep32@gmail.com>
2024-06-13 13:01:32 +0530

    for loop

--- src/exercises/Excercise1.js
+++ src/exercises/Excercise1.js
@@ -2,14 +2,12 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 const Excercise1 = () => {
-  let a = 8;
-  let b = 8;
-  console.log(a == b);
-  console.log(a != b);
-  console.log(a > b);
-  console.log(a >= b);
-  console.log(a < b);
-  console.log(a <= b);
+  let a = 89;
+  if (a > 18) {
+    console.log("you are eligible for vote");
+  } else {
+    console.log("you are not eligible");
+  }
   return (
     <View>
       <Text>Excercise1</Text>
