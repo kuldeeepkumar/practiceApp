5ee145af0c70b62e50aa9fa04b86d1e62be5bdcd
deepjot21234 <prabhprabhjot84@gmail.com>
2024-06-14 09:34:44 +0530

    new exercise one file is created

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Exercise1 from './src/exercises/Exercise1';
+
+
 
 export default function App() {
   console.log("meenakshi")
 
   return (
-    <View style={styles.container}>
+<>
+
+<View style={styles.container}>
       <Text>hello</Text>
       <StatusBar style="auto" />
     </View>
+<Exercise1/>
+</>
   );
 }
 
