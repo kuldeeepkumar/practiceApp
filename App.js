bbfb4ab2c416dc03099354c075dc740de6777576
Rajveer421 <rajveersingh57594@gmail.com>
2024-06-14 13:58:37 +0530

    exercise2 file added to the main file

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Exercise1 from "./src/exercises/Exercise1";
+import Exercise2 from "./src/exercises/exercise2";
 
 export default function App() {
   return (
     <>
       <Exercise1 />
-      <
-      
+      <Exercise2 />
     </>
   );
 }
--- src/exercises/exercise2.js
+++ src/exercises/exercise2.js
@@ -1,14 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-const exercise2 = () => {
+const Exercise2 = () => {
   return (
     <View>
-      <Text>exercise2</Text>
+      <Text>Exercise2</Text>
     </View>
   );
 };
 
-export default exercise2;
+export default Exercise2;
 
 const styles = StyleSheet.create({});
