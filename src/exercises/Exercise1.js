1afbdd229eea0e96110e9d5fb566a0a53f6b9994
amanjotkaur9115 <amanjotkauer1746>
2024-06-14 11:07:33 +0530

    condtion stetments

--- src/exercises/Exercise1.js
+++ src/exercises/Exercise1.js
@@ -37,7 +37,51 @@ const Exerciese1 = () => {
       console.log("match");
       break;
     default:
+      console.log("not match");
       break;
+  }
+  //for loop code is here
+
+  for (let b = 1; b <= 6; b++) {
+    console.log("for execution : ", b);
+  }
+
+  // while loop code is here
+
+  let i = 0;
+  while (i < 5) {
+    console.log("While execution :", i);
+    i++;
+  }
+  // do while loop code is here
+
+  let p = 0;
+  do {
+    console.log("do while execution :", p);
+    p++;
+  } while (p < 5);
+
+  // for in while loop code is here
+
+  const person = {
+    name: "John",
+    age: 30,
+    city: "New York",
+    mobileno: 645567498,
+    email: "john@gmail.com",
+    hobby: "music",
+  };
+
+  for (let key in person) {
+    console.log(key + ": " + person[key]);
+  }
+
+  // for of loop code is here
+
+  const colors = ["red", "green", "blue", "yellow"];
+
+  for (let key of colors) {
+    console.log(key);
   }
 
   return (
