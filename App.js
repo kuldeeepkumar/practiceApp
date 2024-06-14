3f672f930d0763dc624800395b566b7c35aacd10
pinkydevi12 <pinkybhatia48@gmail.com>
2024-06-14 12:55:46 +0530

    condition statement and looping statement are done

--- App.js
+++ App.js
@@ -1,63 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Exercise1 from './src/exercises/exercise1';
+import Exercise1 from './src/exercises/Exercise1';
+import Exercise2 from './src/exercises/Exercise2';
+
 
 export default function App() {
-  let b = "mehaku";
-   console.log("my besti name:",b)
-let  x=20;
-let y=30;
-let a=6;
-console.log("here is sum:",x+y);
-console.log("here is subtraction:",x-y);
-console.log("here is multiplication:",x*y);
-console.log("here is divison:",x/y);
-console.log("here is modules:",x%y);
-console.log("greater than:",x>y);
-console.log("less than:",x<y);
-console.log("equal to:",x==y);
-console.log("not equal to:",x!=y);
-console.log("here is unary plus:",+a);
-console.log("here is unary minus:",-a);
-console.log("here is increment:",++a);
-console.log("here is:decremant",--a);
-let datevar=new Date();
-console.log(datevar);
-let objectvar={name:"pinky",frnd:"ashu"};
-console.log(objectvar);
-let arrayvar=[1,2,3,4];
-console.log(arrayvar);
-let numbervar=234566;
-let stringvar="akshu";
-let booleanvar=true;
-let nullvar=null ;
-console.log(numbervar);
-console.log(stringvar);
-console.log(booleanvar);
-console.log(nullvar);
-let p=10;
-console.log(p+=5);
-console.log(p-=5);
-console.log(p*=1);
-console.log(p/=4);
-let k=true;
-let l=false;
-console.log(k&&l);
-console.log(k||l);
-console.log(!k);
+
+
 
 return (
-<>
-<View style={styles.container}>
-      <Text>jass</Text>
-      <Text>pinko</Text>
-      <Text>ashuu</Text>
-      <StatusBar style="auto" />
-    </View>
-    <Exercise1/>
-</>
-  );
-}
+ <>
+ <Exercise1/>
+<Exercise2/>
+ </>
+
+);
+}  
+
 
 const styles = StyleSheet.create({
   container: {
@@ -67,3 +26,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+console;
--- src/exercises/Exercise1.js
+++ src/exercises/Exercise1.js
@@ -2,17 +2,154 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const Exercise1 = () => {
-    const a = "pinky"
+  let  x=20;
+let y=30;
+let a=6;
+console.log("here is sum:",x+y);
+console.log("here is subtraction:",x-y);
+console.log("here is multiplication:",x*y);
+console.log("here is divison:",x/y);
+console.log("here is modules:",x%y);
+console.log("greater than:",x>y);
+console.log("less than:",x<y);
+console.log("equal to:",x==y);
+console.log("not equal to:",x!=y);
+console.log("here is unary plus:",+a);
+console.log("here is unary minus:",-a);
+console.log("here is increment:",++a);
+console.log("here is:decremant",--a);
+let datevar=new Date();
+console.log(datevar);
+let objectvar={name:"pinky",frnd:"ashu"};
+console.log(objectvar);
+let arrayvar=[1,2,3,4];
+console.log(arrayvar);
+let numbervar=234566;
+let stringvar="akshu";
+let booleanvar=true;
+let nullvar=null ;
+console.log(numbervar);
+console.log(stringvar);
+console.log(booleanvar);
+console.log(nullvar);
+let pp=10;
+console.log(pp+=5);
+console.log(pp-=5);
+console.log(pp*=1);
+console.log(pp/=4);
+let k=true;
+let l=false;
+console.log(k&&l);
+console.log(k||l);
+console.log(!k);
+    
 
-    console.log("value" , a);
+    
+    // If condition code is here
+  const aa = 5;
+  if (aa > 4) {
+    console.log("am here ");
+  }
+  // if else condition code is here
+
+   if (a > 5) {
+   console.log("block one is working");
+   } else {
+     console.log("block two is working");
+   }
+
+  // else if condition code is here
+
+  if (a > 5) {
+    console.log("block one is working ");
+  } else if (a > 6) {
+    console.log("block two is working");
+  } else if (a > 10) {
+    console.log("block three is working");
+  } else {
+    console.log("nothing is working");
+  }
+  // switch statement code is here
+
+  const abc = "Sunday";
+
+  switch (abc) {
+    case "Monday":
+      console.log("Monday is here");
+      break;
+    case "Tuesday":
+      console.log("Tuesday is here");
+      break;
+    case "Wednessday":
+      console.log("Wednessday is here");
+      break;
+    case "Thursday":
+      console.log("Thursday is here");
+      break;
+    case "Friday":
+      console.log("Friday is here");
+      break;
+    case "Saturday":
+      console.log("Saturday is here");
+      break;
+    case "Sunday":
+      console.log("Sunday is here");
+      break;
+    default:
+      break;
+  }
+   // for loop code is here
+
+   for (let b = 1; b <= 6; b++) {
+    console.log("for execution : ", b);
+  }
+
+  // while loop code is here
+
+  let i = 0;
+  while (i < 5) {
+    console.log("While execution :", i);
+    i++;
+  }
+  // do while loop code is here
+
+  let p = 0;
+  do {
+    console.log("do while execution :", p);
+    p++;
+  } while (p < 5);
+
+  // for in while loop code is here
+
+  const person = {
+    name: "John",
+    age: 30,
+    city: "New York",
+    mobileno: 645567498,
+    email: "john@gmail.com",
+    hobby: "music",
+  };
+
+  for (let key in person) {
+    console.log(key + ": " + person[key]);
+  }
+  // for of loop code is here
+
+  const colors = ["red", "green", "blue", "yellow"];
+
+  for (let key of colors) {
+    console.log(key);
+  }
+
+
     
     
   return (
     <View>
       <Text>Exercise1</Text>
     </View>
-  )
-}
+  );
+};
 
 export default Exercise1
 
