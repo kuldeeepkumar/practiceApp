70bb42d1b7a874be3d21b861917456a257bf4ca5
deepak9876462954 <bhatiadeep32@gmail.com>
2024-06-14 16:57:09 +0530

    function declaration done

--- src/exercises/Excercise2.js
+++ src/exercises/Excercise2.js
@@ -1,14 +1,42 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { TouchableOpacity } from "react-native";
+import { yourFunction } from "./Excercise1";
 
 const Excercise2 = () => {
+  function myFuntion(item) {
+    let a = 3;
+    if (a > 2) {
+      console.log("sum", a + item);
+    }
+  }
+
   return (
-    <View>
-      <Text>Excercise2</Text>
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          myFuntion();
+          yourFunction(10);
+        }}
+      >
+        <Text>Excercise2</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default Excercise2;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    backgroundColor: "pink",
+    padding: 10,
+  },
+});
