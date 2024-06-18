9c7af6fdee068149ec54f0d74498d5469d542a20
neharajrani <nehaunchibassi91@gmail.com>
2024-06-18 13:03:18 +0530

    array manipulation

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import Exercise1 from "./src/exercises/Exercise1";
+import Exercise3 from "./src/exercises/Exercise3";
 
 export default function App() {
   return (
     <>
-      {/* <Exercise1 /> */}
-      <Exercise2 />
+      <Exercise3 />
     </>
   );
 }
