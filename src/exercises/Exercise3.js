4170c8ea3e0cff6dd1614ac88c4844c96f366f21
deepjot21234 <prabhprabhjot84@gmail.com>
2024-06-18 11:09:26 +0530

    label change

---
+++ src/exercises/Exercise3.js
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Exercise3 = () => {
+
+
+let fruits=["apple","orange","mango",];
+const fruit=fruits.at(2);
+]
+
+
+
+
+
+
+
+
+
+  return (
+    <View>
+      <Text>Exercise3</Text>
+    </View>
+  )
+}
+
+export default Exercise3
+
+const styles = StyleSheet.create({})
