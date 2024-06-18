63474fd317c1df43cd8f8a1b43d137a2754e6ea8
pinkydevi12 <pinkybhatia48@gmail.com>
2024-06-18 11:46:22 +0530

    array manipulation

---
+++ src/exercises/Exercise3.js
@@ -0,0 +1,44 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Exercise3 = () => {
+  //const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const colors = ["pink", "orange", "yellow", "blue"];
+  //let color = colors.length;
+  //console.log("array length:", color);
+  //console.log(numbers);
+
+  //----------------------->>>>>>>>>>>>>> add Array elememt<<<<<<<<<<<<<<--------------------
+  //push method is perform
+  //colors.push("date");
+  //console.log("my array is here:", colors);
+  //unshift method is perform
+  //colors.unshift("ashu");
+  //console.log("is here:", colors);
+  //adding element at specific postion
+  colors.splice(6, 0, "gray");
+  //console.log("splice method is here:", colors);
+  //----------------------->>>>>>>>>>>>>>Remove array elememt<<<<<<<<<<<<<<--------------------
+
+  colors.pop();
+  // console.log("pop method is here:", colors);
+  //shift method remove the first element
+  colors.shift();
+  //console.log("pop method is here:", colors);
+  colors.splice(2);
+  // console.log("splice method is here:", colors);
+  //----------------------->>>>>>>>>>>>>> Update array elememt<<<<<<<<<<<<<<--------------------
+
+  colors[2] = "black";
+  console.log("update  method is here:", colors);
+
+  return (
+    <View>
+      <Text>Exercise3</Text>
+    </View>
+  );
+};
+
+export default Exercise3;
+
+const styles = StyleSheet.create({});
