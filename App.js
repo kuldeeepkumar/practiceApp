1c6e2bf35a2d7dcf0dfff473f0f119bf8ee92cbd
gurjinder2005 <anjli3118@gmail.com>
2024-06-18 12:35:23 +0530

    array manuplation done

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Excercise1 from "./src/excercises/Excercise1";
 import Excercise2 from "./src/excercises/Excercise2";
+import Excercise3 from "./src/excercises/Excercise3";
 
 export default function App() {
   return (
     <>
       {/* <Excercise1 /> */}
-      <Excercise2 />
+      {/* <Excercise2 /> */}
+      <Excercise3 />
     </>
   );
 }
