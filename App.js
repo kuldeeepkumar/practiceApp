e1fab7089deea5c7a526a73d5df21bf349903f80
gurjinder2005 <anjli3118@gmail.com>
2024-06-18 16:27:55 +0530

    array traversel  done

--- App.js
+++ App.js
@@ -3,13 +3,15 @@ import { StyleSheet, Text, View } from "react-native";
 import Excercise1 from "./src/excercises/Excercise1";
 import Excercise2 from "./src/excercises/Excercise2";
 import Excercise3 from "./src/excercises/Excercise3";
+import Excercise4 from "./src/excercises/Excercise4";
 
 export default function App() {
   return (
     <>
       {/* <Excercise1 /> */}
       {/* <Excercise2 /> */}
-      <Excercise3 />
+      {/* <Excercise3 /> */}
+      <Excercise4 />
     </>
   );
 }
