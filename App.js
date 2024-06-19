fd4088d749b10bcdf55a60b1867803b200db7bd5
Kuldeep <pro.kuldeepkumar@gmail.como>
2024-06-19 15:11:43 +0530

    ES6 and modern Java script Features are done

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import Exercise2 from "./src/exercises/Exercise2";
 import Exercise3 from "./src/exercises/Exercise3";
 import Exercise4 from "./src/exercises/Exercise4";
 import Exercise5 from "./src/exercises/Exercise5";
+import Exercise6 from "./src/exercises/Exercise6";
 import { StatusBar } from "expo-status-bar";
 import { kuldeep } from "./src/components/index";
 
@@ -25,10 +26,12 @@ export default function App() {
       {/* <Exercise4 /> */}
 
       {/* Error handling and debugging techniques are covered in Exercise 5  */}
-      <Exercise5
+      {/* <Exercise5
         title={"Exercise5"}
         subtitle={"Exercise 5 is about error handling and debugging"}
-      />
+      /> */}
+      {/* •	ES6 and modern Java script Features are covered in Exercise 6  */}
+      <Exercise6 />
     </>
   );
 }
