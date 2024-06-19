0c2a72d9cc912a70b3650d58024f7e0a35f651ce
sharmaaashima <aashimasharma164@gmail.com>
2024-06-19 10:19:30 +0530

    array traversal ex4

--- src.js/exercises/Exercise3.js
+++ src.js/exercises/Exercise3.js
@@ -1,61 +1,61 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 const Exercise3 = () => {
-    let fruits = ["apple", "mango", "oranges"];
-    const numbers = [1,2,3,4,4,5];
-    const color  = ["red","pink","blue", "purple", "white"];
-    console.log("is here numbers",numbers);
-    const array = [1,2,3,4,5];
-    console.log("array length", color);
-    //push method is here
-    numbers.push[2];
-    console.log("is here color",numbers);
-    //unshift method is here
-    color.unshift("blue");
-    console.log("My array is here",color);
-    fruits.unshift("aaple");
-     console.log("is here",fruits);
-    //adding element is speflic position
-    color.splice(2, 3 , "yellow");
-    console.log("is here", color);
+  let fruits = ["apple", "mango", "oranges"];
+  const numbers = [1, 2, 3, 4, 4, 5];
+  const color = ["red", "pink", "blue", "purple", "white"];
+  console.log("is here numbers", numbers);
+  const array = [1, 2, 3, 4, 5];
+  console.log("array length", color);
+  //push method is here
+  numbers.push[2];
+  console.log("is here color", numbers);
+  //unshift method is here
+  color.unshift("blue");
+  console.log("My array is here", color);
+  fruits.unshift("aaple");
+  console.log("is here", fruits);
+  //adding element is speflic position
+  color.splice(2, 3, "yellow");
+  console.log("is here", color);
 
-    //removing elements from the end
-    fruits.pop("mango");
-    console.log("is here", fruits);
+  //removing elements from the end
+  fruits.pop("mango");
+  console.log("is here", fruits);
 
-    //removing elements at the specific positions
-    color.splice(2, 3);
-    console.log("is here", color);
+  //removing elements at the specific positions
+  color.splice(2, 3);
+  console.log("is here", color);
 
-    //updating an element
-    fruits[1] = "litchi";
-    console.log("fruits");
+  //updating an element
+  fruits[1] = "litchi";
+  console.log("fruits");
 
-    for (let i = 0; i < array.length; i++){
-      console.log("array is here", array);
-      console.log("i is here", i);
-      console.log("length", array.length);
-    }
+  for (let i = 0; i < array.length; i++) {
+    console.log("array is here", array);
+    console.log("i is here", i);
+    console.log("length", array.length);
+  }
 
-    //using forEach
-    fruits.forEach(fruit  => {
-      console.log(fruit);
-})
+  //using forEach
+  fruits.forEach((fruit) => {
+    console.log(fruit);
+  });
 
-    //using for...of loop:
+  //using for...of loop:
 
-    for (const fruit of fruits) {
-      console.log(fruit);
-    }
+  for (const fruit of fruits) {
+    console.log(fruit);
+  }
 
   return (
     <View>
       <Text>Exercise3</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Exercise3
+export default Exercise3;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- src.js/exercises/Exercise4.js
+++ src.js/exercises/Exercise4.js
@@ -1,51 +1,61 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 const Exercise4 = () => {
-     const colors=["pink","yellow","blue"]
-    //using forEach 
-    const array1 = [
-        {
-            name: "abc",
-            city: "jalander",
-         },
-        {
-         name: "abc",
-         city: "mukerian",
-         },
+  const colors = ["pink", "yellow", "blue"];
+  //using forEach
+  const array1 = [
     {
-          name: "abc",
-          city: "dasuya", 
-        
-         },
-    ];
-    
-         array1.forEach((elements) => {
-            console.log("element value", elements.city); // Access array elements using element
-});
+      name: "abc",
+      city: "jalander",
+    },
+    {
+      name: "abc",
+      city: "mukerian",
+    },
+    {
+      name: "abc",
+      city: "dasuya",
+    },
+  ];
 
-    
+  array1.forEach((elements) => {
+    console.log("element value", elements.city); // Access array elements using element
+  });
+
+  //using array method (e.g.,filter,reduce,etc)
+  const number = [1, 2, 3, 4, 5];
+
+  const sum = number.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    0
+  );
+  console.log(sum);
+
+  //using map to transform elmenets
+  let uppercasedcolors = colors.map((color) => color.toUpperCase());
+  console.log(uppercasedcolors);
+
   return (
     <View style={styles.container}>
-    {colors.map((item) => (
-      <>
-        <Text style={styles.button}>{item}</Text>
-      </>
-    ))}
-  </View>
-);
+      {colors.map((item) => (
+        <>
+          <Text style={styles.button}>{item}</Text>
+        </>
+      ))}
+    </View>
+  );
 };
 export default Exercise4;
 
 const styles = StyleSheet.create({
-container: {
-  flex: 1,
-  backgroundColor: "#fff",
-  alignItems: "center",
-  justifyContent: "center",
-},
-button: {
-  marginHorizontal: 20,
-},
-    
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    marginHorizontal: 20,
+  },
 });
