163878270651e320273c634c0693932e18af88b9
shivanidahiya127 <shivanidahiya127@gmail.com>
2024-06-19 15:50:46 +0530

    ES6 and modern javascript features are done

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import Exercise2 from "./src/exercises/Exercise2";
 import Exercise3 from "./src/exercises/Exercise3";
 import Exercise4 from "./src/exercises/Exercise4";
 import Exercise5 from "./src/exercises/Exercise5";
+import Exercise6 from "./src/exercises/Exercise6";
 export default function App() {
   return (
     <>
@@ -12,12 +13,13 @@ export default function App() {
       {/* <Exercise2 /> */}
       {/* <Exercise3 /> */}
       {/* <Exercise4 /> */}
-      {
+      {/*
         <Exercise5
           title={"Exercise5"}
           subtitle={"Exercise 5 is about error handling and debugging"}
-        />
-      }
+        /> 
+      */}
+      {<Exercise6 />}
     </>
   );
 }
