c5c81c3edd48ad179d06538a2424dcda378f58cd
amanjotkaur9115 <amanjotkaur1746@gmail.com>
2024-06-19 13:01:55 +0530

    Error Handling and Debugging is done

--- App.js
+++ App.js
@@ -3,13 +3,20 @@ import { StyleSheet, Text, View } from "react-native";
 import Exercise1 from "./src/Exercise/Exercise1";
 import Exercise2 from "./src/Exercise/Exercise2";
 import Exercise3 from "./src/Exercise/Exercise3";
+import Exercise4 from "./src/Exercise/Exercise4";
+import Exercise5 from "./src/Exercise/Exercise5";
 
 export default function App() {
   return (
     <>
       {/*<Exercise1>*/}
       {/* <Exercise2 /> */}
-      <Exercise3 />
+      {/* <Exercise3 /> */}
+      {/* <Exercise4 /> */}
+      <Exercise5
+        title={"Exercise5"}
+        subtitle={"Exercise 5 is about error handling and debugging"}
+      />
     </>
   );
 }
