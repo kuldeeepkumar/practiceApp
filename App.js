a1d3cb5bee90e6c83c4ef64e08fb0f3175149504
pinkydevi12 <pinkybhatia48@gmail.com>
2024-06-19 12:10:21 +0530

    Error Handling and debugging done

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import Exercise1 from "./src/exercises/Exercise1";
 import Exercise2 from "./src/exercises/Exercise2";
 import Exercise3 from "./src/exercises/Exercise3";
 import Exercise4 from "./src/exercises/Exercise4";
+import Exercise5 from "./src/exercises/Exercise5";
 
 export default function App() {
   return (
@@ -11,7 +12,11 @@ export default function App() {
       {/* <Exercise1 /> */}
       {/*<Exercise2 /> */}
       {/*<Exercise3 /> */}
-      <Exercise4 />
+      {/*<Exercise4 /> */}
+      <Exercise5
+        title={"Exercise5"}
+        subtitle={"Error handling and debugging"}
+      />
     </>
   );
 }
---
+++ src/exercises/Exercise5.js
@@ -0,0 +1,73 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Exercise5 = ({ title, subtitle }) => {
+  // 1. syntax error
+  let a = 20;
+  console.log(a);
+  //missing closing parenthesis in console.log statemennt
+
+  //2.reference error
+  console.log(y);
+  let y = 10;
+
+  //3. Type Error
+  let z = null;
+  z.toUpperCase();
+
+  //--------------------->>>>>>>>>>>>>>debugging techniques<<<<<<<<<<<<<<<<-------------------
+  let name = "janvibhatia";
+  console.log("Name:", name);
+
+  //using debugger
+
+  function add() {
+    let b = 10;
+    debugger;
+    console.log(b + 5);
+  }
+  add();
+
+  //using try and catch block
+  try {
+    let result = undefinedvariable * 2;
+    console.log("Result:", result);
+  } catch (error) {
+    console.log("Error", error.message);
+  }
+
+  //using throw statement
+  function divide(r, q) {
+    if (r === 0) {
+      throw new error("division by 0");
+    }
+    return r / q;
+  }
+  try {
+    console.log(divide(10, 5));
+  } catch (error) {
+    console.log("Error", error.message);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>{title}</Text>
+      <Text>{subtitle}</Text>
+    </View>
+  );
+};
+
+export default Exercise5;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    backgroundColor: "pink",
+    padding: 15,
+  },
+});
