2a6b9a83b5672c4375da2b2af7793c72acf8c5a8
Kuldeep <pro.kuldeepkumar@gmail.como>
2024-06-20 14:56:29 +0530

    flex box and position absolute and position relative design understanding done

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-
 import Exercise1 from "./src/exercises/Exercise1";
 import Exercise2 from "./src/exercises/Exercise2";
 import Exercise3 from "./src/exercises/Exercise3";
 import Exercise4 from "./src/exercises/Exercise4";
 import Exercise5 from "./src/exercises/Exercise5";
 import Exercise6 from "./src/exercises/Exercise6";
+import Exercise7 from "./src/exercises/Exercise7";
+import Exercise8 from "./src/exercises/Exercise8";
+import Exercise9 from "./src/exercises/Exercise9";
 import { StatusBar } from "expo-status-bar";
-import { kuldeep } from "./src/components/index";
+import { StyleSheet } from "react-native";
 
 export default function App() {
   return (
@@ -30,8 +31,18 @@ export default function App() {
         title={"Exercise5"}
         subtitle={"Exercise 5 is about error handling and debugging"}
       /> */}
-      {/* •	ES6 and modern Java script Features are covered in Exercise 6  */}
-      <Exercise6 />
+
+      {/* ES6 and modern Java script Features are covered in Exercise 6  */}
+      {/* <Exercise6 /> */}
+
+      {/*	Using flex properties: flex, flexDirection, justifyContent, alignItems are covered in Exercise 7 */}
+      {/* <Exercise7 /> */}
+
+      {/* 	Building responsive layouts with Flexbox are covered in Exercise 8 */}
+      {/* <Exercise8 /> */}
+
+      {/* Styling with absolute and relative positioning are covered in Exercise 9 */}
+      <Exercise9 />
     </>
   );
 }
