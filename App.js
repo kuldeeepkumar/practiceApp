3b26a0a3afe05b64b175218e63293de6ac212523
hasandeep3013 <singhhasandeep22@gmail.com>
2024-06-20 13:53:31 +0530

    css

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Exercise1 from "./src/exercises/Exercise1";
 import Exercise2 from "./src/exercises/Exercise2";
+import Exercise7 from "./src/exercises/Exercise7";
 
 export default function App() {
   return (
     <>
       <Exercise1 />
       {/*/<Exercise2 />*/}
+      <Exercise7 />
     </>
   );
 }
