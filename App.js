7a23cf9c5264c6f824ef97894321e27aafb30d76
deepjot21234 <prabhprabhjot84@gmail.com>
2024-06-20 14:20:39 +0530

    flex properties in react native is done

--- App.js
+++ App.js
@@ -6,18 +6,21 @@ import Exercise3 from "./src/exercises/Exercise3";
 import Exercise4 from "./src/exercises/Exercise4";
 import Exercise5 from "./src/exercises/Exercise5";
 import Exercise6 from "./src/exercises/Exercise6";
+import Exercise7 from "./src/exercises/exercise7";
+
 export default function App() {
   return (
     <>
-      {/* <Exercise1 /> */}
-      {/*  <Exercise2 /> */}
+      {/* <Exercise1/> */}
+      {/* <Exercise2/> */}
       {/* <Exercise3/> */}
       {/* <Exercise4/> */}
-      {/* <Exercise5
-        title={"Exercise6"}
-        subtitle={"Exercise 6 is about error handling and debugging"}
+      {/* <Exercise5/> */}
+      {/* <Exercise6
+        title={"Exercise7"}
+        subtitle={"Exercise 7 is about Using flex properties in React native is done and stylesheet understa…"}
       /> */}
-      <Exercise6 />
+      <Exercise7 />
     </>
   );
 }
