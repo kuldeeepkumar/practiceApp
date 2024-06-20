d95a391d65adb3c5518cccb6ed9ef8a1c31e863f
neharajrani <nehaunchibassi91@gmail.com>
2024-06-20 12:41:00 +0530

    react native and style sheet

---
+++ src/exercises/Exercise7.js
@@ -0,0 +1,47 @@
+import { StyleSheet, Text, View } from "react-native";
+
+import React from "react";
+
+const Exercise7 = () => {
+  return (
+    <View style={styles1.container}>
+      <View style={styles1.box}>
+        <Text style={styles1.text}>Box 1</Text>
+      </View>
+      <View style={[styles1.box, { backgroundColor: "green" }]}>
+        <Text style={styles1.text}>Box 2</Text>
+      </View>
+      <View style={[styles1.box, { backgroundColor: "blue" }]}>
+        <Text style={styles1.text}>Box 3</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Exercise7;
+
+// -->>>>>>>>>>>  Using flex properties: flex, flexDirection, justifyContent, alignItems <<<<<<<<<<<<<---
+
+const styles1 = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row", // Try changing to 'column' and see the difference
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    backgroundColor: "#46c2ab",
+  },
+  box: {
+    width: 120,
+    height: 120,
+    backgroundColor: "red",
+    justifyContent: "center",
+    alignItems: "center",
+    borderTopEndRadius: 10,
+    borderColor: "white",
+    borderWidth: 20,
+  },
+  text: {
+    fontSize: 20,
+    color: "black",
+  },
+});
