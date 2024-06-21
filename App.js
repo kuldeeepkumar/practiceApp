ec8dc5effba737046e2500d68f137d7b1622a9c6
sandhushweta <sandhushweta1408@gmail.com>
2024-06-21 14:09:28 +0530

    jaahhaajjj

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import Exercise6 from './scr/exercises/Exercise6';
 import Exercise7 from "./scr/exercises/Exercise7";
 import Exercise8 from "./scr/exercises/Exercise8";
 import Exercise9 from "./scr/exercises/Exercise9";
+import Exercise10 from "./scr/exercises/Exercise10";
 
 
 
@@ -23,8 +24,9 @@ export default function App() {
        {/*<Exercise6/> */}
        {/*<Exercise7/> */}
        {/*<Exercise8/> */}
-       <Exercise9/>       
-           
+       {/*<Exercise9/> */}
+       <Exercise10/>       
+            
 
       
     </>
