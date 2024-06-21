b2bce0eabea544e91427b09af0daf07f50b114f8
Rajveer421 <rajveersingh57594@gmail.com>
2024-06-21 12:59:35 +0530

    ex10

--- App.js
+++ App.js
@@ -9,7 +9,8 @@ import Exercise5 from "./src/exercises/Exercise5";
 import Exercise6 from "./src/exercises/Exercise6";
 import Exercise7 from "./src/exercises/Exercise7";
 import Exercise8 from "./src/exercises/Exercise8";
-import Exercise9 from "./Exercise9";
+import Exercise9 from "./src/exercises/Exercise9";
+import Exercise10 from "./src/exercises/Exercise10";
 
 export default function App() {
   return (
@@ -23,7 +24,8 @@ export default function App() {
       {/* <Exercise6 /> */}
       {/* <Exercise7 /> */}
       {/* <Exercise8 /> */}
-      <Exercise9 />
+      {/* <Exercise9 /> */}
+      <Exercise10 />
     </>
   );
 }
