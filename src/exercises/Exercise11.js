8dd20b92e9199b9fea9ab1e7bca91f421c15fb3e
sharmaaashim <aashimasharma164gmail.com>
2024-06-21 14:32:58 +0530

    some changes in multidedia and images component exercise

--- src/exercises/Exercise11.js
+++ src/exercises/Exercise11.js
@@ -1,26 +1,23 @@
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
-
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
 
 const Exercise11 = () => {
   return (
-      <View style={styles.container}>
- <TouchableOpacity>
-
+    <View style={styles.container}>
+      <TouchableOpacity>
         <Image
           style={styles.image}
           source={{
-            uri: "https://picsum.photos/200",
+            uri: "https://cdn.pixabay.com/photo/2021/01/01/06/47/watermelon-5877895_1280.png",
           }}
         />
- </TouchableOpacity>
-
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-     // ---->>>>>>>>>>> Styling images and multimedia components <<<<<<<<<<<<<-----
+  // ---->>>>>>>>>>> Styling images and multimedia components <<<<<<<<<<<<<-----
   container: {
     flex: 1,
     justifyContent: "center",
