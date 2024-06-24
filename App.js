9822c3cde4464a183c9c34a8c880d081753a3940
amanjotkaur9115 <amanjotkaur1746>
2024-06-24 15:58:24 +0530

    Some file structure corrections are done and reusable components and styles are practiced

--- App.js
+++ App.js
@@ -1,16 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import Exercise1 from "./src/Exercise/Exercise1";
-import Exercise2 from "./src/Exercise/Exercise2";
-import Exercise3 from "./src/Exercise/Exercise3";
-import Exercise4 from "./src/Exercise/Exercise4";
-import Exercise5 from "./src/Exercise/Exercise5";
-import Exercise6 from "./src/Exercise/Exercise6";
-import Exercise7 from "./src/Exercise/Exercise7";
-import Exercise8 from "./src/Exercise/Exercise8";
-import Exercise9 from "./src/Exercise/Exercise9";
-import Exercise10 from "./src/Exercise/Exercise10";
-import Exercise11 from "./src/Exercise/Exercise11";
+
+import Exercise12 from "./src/exercises/Exercise12";
 
 export default function App() {
   return (
@@ -25,7 +16,8 @@ export default function App() {
       {/* <Exercise8 /> */}
       {/* <Exercise9 /> */}
       {/* <Exercise10 />*/}
-      <Exercise11 />
+      {/* <Exercise11 />*/}
+      <Exercise12 />
     </>
   );
 }
---
+++ src/exercises/Exercise12.js
@@ -0,0 +1,20 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+import React from "react";
+import { containerStyle } from "../styles";
+import ReusableButton from "../components/ReusableButton";
+
+const Exercise12 = () => {
+  const handleClick = () => {
+    alert("buttons is clicked");
+  };
+  return (
+    <View style={containerStyle.container}>
+      <ReusableButton onClick={handleClick} title="Home" />
+      <ReusableButton title="Profile" />
+      <ReusableButton title="settings" />
+    </View>
+  );
+};
+
+export default Exercise12;
