b6d5dc68b554a14e276f53f210d590c17fd1b1df
deepjot21234 <prabhprabhjot84@gmail.com>
2024-06-25 14:37:38 +0530

    button types  are covered

--- App.js
+++ App.js
@@ -6,23 +6,28 @@ import Exercise3 from "./src/exercises/Exercise3";
 import Exercise4 from "./src/exercises/Exercise4";
 import Exercise5 from "./src/exercises/Exercise5";
 import Exercise6 from "./src/exercises/Exercise6";
-import Exercise7 from "./src/exercises/exercise7";
-import exercise8 from "./src/exercises/exercise8";
+import Exercise7 from "./src/exercises/Exercise7";
+import Exercise8 from "./src/exercises/Exercise8";
+import Exercise9 from "./src/exercises/Exercise9";
+import Exercise10 from "./src/exercises/Exercise10";
 
 export default function App() {
   return (
     <>
+      {/* Variable declaration , datatypes , operators , conditions statements and
+      looping statements are done using following component */}
       {/* <Exercise1/> */}
+
+      {/* Funtion Declaration  */}
       {/* <Exercise2/> */}
       {/* <Exercise3/> */}
       {/* <Exercise4/> */}
       {/* <Exercise5/> */}
       {/* <Exercise6/> */}
-      {/* <Exercise7
-        title={"Exercise8"}
-        subtitle={"Exercise 8 is about Using flex box and position absolute and position relative design understan…"}
-      /> */}
-      <Exercise8 />
+      {/* <Exercise7/> */}
+      {/* <Exercise8/> */}
+      {/* <Exercise9 />*/}
+      <Exercise10 />
     </>
   );
 }
