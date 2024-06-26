59dcd6169574d01096f5cbdfdd778afd27821d57
neharajrani <nehaunchibassi91@gmail.com>
2024-06-26 11:55:30 +0530

    navigation and list

---
+++ src/exercises/Exercise16.js
@@ -0,0 +1,31 @@
+import { FlatList, Text, View } from "react-native";
+
+import React from "react";
+
+const DATA = [
+  { id: "1", title: "Name 11" },
+  { id: "2", title: "Item 12" },
+  { id: "3", title: "Item 13" },
+  // Add more items
+];
+
+const Item = ({ title }) => (
+  <View>
+    <Text>{title}</Text>
+  </View>
+);
+//  How to Handle list
+
+const Exercise16 = () => {
+  return (
+    <View>
+      <FlatList
+        data={DATA}
+        renderItem={({ item }) => <Item title={item.title} />}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  );
+};
+
+export default Exercise16;
