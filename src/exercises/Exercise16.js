dcb517e7e50930cae3ee811479eceba85af00735
pinkydevi12 <pinkybhatia48@gmail.com>
2024-06-26 13:43:18 +0530

    navigatoin and handle list exercises are done

--- src/exercises/Exercise16.js
+++ src/exercises/Exercise16.js
@@ -1,26 +1,48 @@
 import { FlatList, Text, View } from "react-native";
 import React from "react";
+import { containerStyle } from "../styles";
 const DATA = [
-  { id: "1", title: "Item 1" },
-  { id: "2", title: "Item 2" },
-  { id: "3", title: "Item 3" },
+  { id: "1", title: "apple" },
+  { id: "2", title: "Mango" },
+  { id: "3", title: "cherry" },
+  { id: "4", title: "cherry" },
+  { id: "5", title: "cherry" },
+  { id: "6", title: "cherry" },
+  { id: "7", title: "cherry" },
+  { id: "8", title: "cherry" },
+  { id: "9", title: "cherry" },
   // Add more items
 ];
 
 const Item = ({ title }) => (
-  <View>
-    <Text>{title}</Text>
+  <View
+    style={{
+      borderWidth: 3,
+      borderColor: "red",
+      marginVertical: 5,
+      width: 250,
+    }}
+  >
+    <Text
+      style={{
+        fontSize: 20,
+        textAlign: "center",
+      }}
+    >
+      👉 {title} 👈
+    </Text>
   </View>
 );
 //  How to Handle list
 
 const Exercise16 = () => {
   return (
-    <View>
+    <View style={containerStyle.container}>
       <FlatList
         data={DATA}
         renderItem={({ item }) => <Item title={item.title} />}
         keyExtractor={(item) => item.id}
+        centerContent
       />
     </View>
   );
