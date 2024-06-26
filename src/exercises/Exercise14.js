619da340ea860060cd0920b5f2356e6e4b09745d
neharajrani <nehaunchibassi91@gmail.com>
2024-06-26 10:46:35 +0530

    useEffect Code

---
+++ src/exercises/Exercise14.js
@@ -0,0 +1,36 @@
+import { Button, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { containerStyle } from "./styles/Index";
+
+const Exercise14 = () => {
+  // ------------------>>>>>>>>>>>>>>>>>>> Hooks and Lifecycle <<<<<<<<<<<<<<<<<<--------------------------
+
+  const [count, setCount] = useState(0);
+
+  const [name, setName] = useState("");
+  console.log("name is here ", name);
+
+  useEffect(() => {
+    alert("Component mounted or count updated");
+    return () => {
+      alert("Component will unmount or count will update");
+    };
+  }, [count]);
+
+  return (
+    <View style={containerStyle.container}>
+      <Text> Hi my name is {name}</Text>
+      <Text>Count: {count}</Text>
+      <Button
+        title="Increment"
+        onPress={() => {
+          {
+            setCount(count + 1), setName("Deepak");
+          }
+        }}
+      />
+    </View>
+  );
+};
+
+export default Exercise14;
