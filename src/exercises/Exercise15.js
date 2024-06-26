43b83a86172f6906100feeae648186d321a0cf63
Abhishekbharti69 <avshekbharti@gmail.com>
2024-06-26 10:42:14 +0530

    hooks and lifecycle

---
+++ src/exercises/Exercise15.js
@@ -0,0 +1,36 @@
+import { Button, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+
+import { containerStyle } from "../styles";
+
+const Exercise15 = () => {
+  // ------------------>>>>>>>>>>>>>>>>>>> Hooks and Lifecycle <<<<<<<<<<<<<<<<<<--------------------------
+
+  const [count, setCount] = useState(0);
+
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    alert("Component mounted or count updated");
+    return () => {
+      alert("Component will unmount or count will update");
+    };
+  }, [count]);
+
+  return (
+    <View style={containerStyle.container}>
+      <Text> Jaddu ii kehde bhai abhiiii {name}</Text>
+      <Text>Count: {count}</Text>
+      <Button
+        title="Increment"
+        onPress={() => {
+          {
+            setCount(count + 1), setName("Deepak");
+          }
+        }}
+      />
+    </View>
+  );
+};
+
+export default Exercise15;
