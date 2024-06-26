1ee81922567cea9dfff7e1da807083d21ba05541
shivanidahiya127 <shivanidahiya127@gmail.com>
2024-06-26 10:45:27 +0530

    useEffect code is done

--- App.js
+++ App.js
@@ -1,25 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import Exercise1 from "./src/exercises/Exercise1";
-import Exercise2 from "./src/exercises/Exercise2";
-import Exercise3 from "./src/exercises/Exercise3";
-import Exercise4 from "./src/exercises/Exercise4";
-import Exercise5 from "./src/exercises/Exercise5";
-import Exercise6 from "./src/exercises/Exercise6";
-import Exercise7 from "./src/exercises/Exercise7";
-import Exercise8 from "./src/exercises/Exercise8";
-import Exercise9 from "./src/exercises/Exercise9";
-import Exercise10 from "./src/exercises/Exercise10";
-import Exercise11 from "./src/exercises/Exercise11";
-import Exercise12 from "./src/exercises/Exercise12";
-import Exercise13 from "./src/exercises/Exercise13";
+import Exercise14 from "./src/exercises/Exercise14";
+
 export default function App() {
   return (
     <>
       {/* <Exercise1 /> */}
       {/*<Exercise2 />*/}
-      {/* <Exercise3 /> */}
-      {/* <Exercise4 /> */}
+      {/* <Exercise3 />
+      <Exercise4 /> */}
       {/*
         <Exercise5
           title={"Exercise5"}
@@ -33,7 +22,8 @@ export default function App() {
       {/*<Exercise10 />*/}
       {/*<Exercise11 />*/}
       {/*<Exercise12 />*/}
-      {<Exercise13 />}
+      {/*<Exercise13 />*/}
+      {<Exercise14 />}
     </>
   );
 }
